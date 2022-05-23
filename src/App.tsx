c6f63e2f891bb995c555c47cd756d66456c9ceac
7inh <tranquoclinh247@gmail.com>
2022-05-24 00:11:10 +0700

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
-import './App.css';
-import Todo from './features/todo/Todo';
+import * as React from "react";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Notfound from "./pages/Notfound";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Todo />
-      </header>
+    <div>
+      <h1>Basic Example</h1>
+
+      <p>
+        This example demonstrates some of the core features of React Router
+        including nested <code>&lt;Route&gt;</code>s,{" "}
+        <code>&lt;Outlet&gt;</code>s, <code>&lt;Link&gt;</code>s, and using a
+        "*" route (aka "splat route") to render a "not found" page when someone
+        visits an unrecognized URL.
+      </p>
+
+      {/* Routes nest inside one another. Nested route paths build upon
+            parent route paths, and nested route elements render inside
+            parent route elements. See the note about <Outlet> below. */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path="*" element={<Notfound />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
-
-export default App;
