3ea68a0c6360bb3d4ececd175cfa1b103264b56d
7inh <tranquoclinh247@gmail.com>
2022-05-27 00:45:21 +0700

    login button

--- src/features/user/User.tsx
+++ src/features/user/User.tsx
@@ -1,7 +1,47 @@
 export default function User() {
   return (
-      <div>
-        user
+    <div className="menu_btn_box jb_cover">
+      <div className="jb_profile_box user-controller img">
+        <div className="nice-select" tabIndex={0}>
+          <span
+            className="current"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <img src="images/pf.png" alt="img" />
+            <div
+              className="luca_profile_wrapper"
+              style={{ lineHeight: "25px" }}
+            >
+              <div style={{ fontSize: 16 }}>Luca Wallace</div>
+              <a href="#0">luca@example.com</a>
+            </div>
+          </span>
+          <ul className="list">
+            <li>
+              <a href="#0">
+                <i className="fas fa-user-edit" />
+                account
+              </a>
+            </li>
+            <li>
+              <a href="#0">
+                <i className="fas fa-cog" />
+                Setting
+              </a>
+            </li>
+            <li>
+              <a href="#0">
+                <i className="fas fa-sign-in-alt" />
+                logout
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
+    </div>
   );
 }
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,8 +1,12 @@
 import { Link, Outlet } from "react-router-dom";
 import Login from "../features/login/Login";
 import { useLocation } from "react-router-dom";
+import User from "../features/user/User";
+import { useSelector } from "react-redux";
+import { selectLogin } from "../features/login/loginSlice";
 
 export default function Layout() {
+  const { token } = useSelector(selectLogin);
   const location = useLocation();
   console.log("pathname", location.pathname);
   return (
@@ -302,47 +306,8 @@ export default function Layout() {
             </div>
             {/* .cd-dropdown-wrapper */}
           </header>
-          {/* <div className="menu_btn_box header_btn jb_cover">
-            <ul>
-              <li></li>
-              <li>
-                <Login />
-              </li>
-            </ul>
-          </div> */}
-          <div className="menu_btn_box jb_cover">
-            <div className="jb_profile_box user-controller img">
-              <div className="nice-select" tabIndex={0}>
-                <span className="current" style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                  <img src="images/pf.png" alt="img" />
-                  <div className="luca_profile_wrapper" style={{lineHeight: "25px"}}>
-                      <div style={{fontSize: 16}}>Luca Wallace</div>
-                      <a href="#0">luca@example.com</a>
-                  </div>
-                </span>
-                <ul className="list">
-                  <li>
-                    <a href="#0">
-                      <i className="fas fa-user-edit" />
-                      account
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#0">
-                      <i className="fas fa-cog" />
-                      Setting
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#0">
-                      <i className="fas fa-sign-in-alt" />
-                      logout
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
+
+          {token ? <User /> : <Login />}
 
           <div className="jb_navigation_wrapper">
             <div className="mainmenu d-xl-block d-lg-block d-md-none d-sm-none d-none">
