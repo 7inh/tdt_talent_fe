aa4ce63ce8666918bf25a5aa5fb123447072714b
Annn64778 <meobong1120@gmail.com>
2022-06-01 22:39:28 +0700

    lam: companylist_fe

---
+++ src/hooks/useCompanyList.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { selectLogin } from "../features/login/loginSlice";
+
+const useCompanyList = () => {
+  const { token } = useSelector(selectLogin);
+  const [company, setCompany] = useState([
+    {
+      full_name: "",
+      address: "",
+    },
+  ]);
+
+  useEffect(() => {
+    const getCompany = async () => {
+      var axios = require("axios");
+
+      var config = {
+        method: "get",
+        url: "http://localhost:7000/api/account/get_all_company",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const response = await axios(config);
+      console.log("response", response.data);
+
+      setCompany(response.data);
+    };
+
+    getCompany();
+  }, [token]);
+
+  return company;
+};
+
+export default useCompanyList;
