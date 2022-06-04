d8a48e257cdfb090c45832ffd68f301318cd8d57
7inh <tranquoclinh247@gmail.com>
2022-06-04 20:04:03 +0700

    update path, simple format

--- src/pages/dashboards/Dashboard.tsx
+++ src/pages/dashboards/Dashboard.tsx
@@ -1,21 +1,23 @@
 import { useSelector } from "react-redux";
 import { selectLogin } from "../../features/login/loginSlice";
-import CandidateDashBoard from "./CandidateDashboard";
-import CompanyDashboard from "./CompanyDashboard";
+import CandidateDashBoard from "./candidate/CandidateDashboard";
+import CompanyDashboard from "./company/CompanyDashboard";
 
 export default function Dashboard() {
-    
   const { user } = useSelector(selectLogin);
-  console.log('user', user)
-    switch (user.role) {
-        case 'company': {
-            return (<>
-                <CompanyDashboard />
-            </>)
-        }
-        default:
-            return (<>
-                <CandidateDashBoard />
-            </>)
+  switch (user.role) {
+    case "company": {
+      return (
+        <>
+          <CompanyDashboard />
+        </>
+      );
     }
+    default:
+      return (
+        <>
+          <CandidateDashBoard />
+        </>
+      );
+  }
 }
