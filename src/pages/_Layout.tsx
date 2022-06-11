1ad27558c4f0670811a583249d83062dc80c798c
7inh <tranquoclinh247@gmail.com>
2022-06-12 01:44:50 +0700

    select role

--- src/pages/_Layout.tsx
+++ src/pages/_Layout.tsx
@@ -1,7 +1,11 @@
+import axios from "axios";
 import { useEffect } from "react";
+import { MdMargin } from "react-icons/md";
+import { useDispatch, useSelector } from "react-redux";
 import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/Footer";
 import Navigation from "../components/Navigation";
+import { selectLogin, setUser } from "../features/login/loginSlice";
 
 declare global {
   interface Window {
@@ -11,7 +15,23 @@ declare global {
 }
 
 export default function Layout() {
+  const dispatch = useDispatch();
   const location = useLocation();
+  const { user, token } = useSelector(selectLogin);
+  const setRole = async (role: string) => {
+    var config = {
+      method: "post",
+      url: "http://localhost:7000/api/account/update/" + role,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: {},
+    };
+
+    const response = await axios(config);
+    dispatch(setUser({ user: response.data }));
+    window.location.reload();
+  };
 
   useEffect(() => {
     window.refreshSelect();
@@ -37,11 +57,54 @@ export default function Layout() {
       {/* Top Scroll End */}
       {/* cp navi wrapper Start */}
 
-      <Navigation />
+      {user.role === "user" ? (
+        <div style={{ height: "100vh" }}>
+          <div
+            style={{
+              fontSize: "36px",
+              textAlign: "center",
+              margin: "100px",
+              color: "#ff3366",
+            }}
+          >
+            Pick role
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              height: "30vh",
+              alignItems: "center",
+              padding: "100px",
+              margin: "100px",
+            }}
+          >
+            <div
+              style={{ cursor: "pointer" }}
+              onClick={() => setRole("candidate")}
+            >
+              Candidate
+            </div>
+            <div
+              style={{ borderLeft: "1px solid #ff3366", height: "inherit" }}
+            ></div>
+            <div
+              style={{ cursor: "pointer" }}
+              onClick={() => setRole("company")}
+            >
+              Company
+            </div>
+          </div>
+        </div>
+      ) : (
+        <>
+          <Navigation />
 
-      <Outlet />
+          <Outlet />
 
-      <Footer />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
