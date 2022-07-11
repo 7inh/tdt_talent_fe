dda850fee98d0127f08466ab8e576bc45e075d35
7inh <tranquoclinh27@gmail.com>
2022-07-11 17:49:27 +0700

    remove

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,8 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import loginSlice from "../features/login/loginSlice";
-import todoSlice from "../features/todo/todoSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-    todo: todoSlice,
     login: loginSlice,
   },
 });
