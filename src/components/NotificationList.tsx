8526f8e66e82bee1e0949c61fbccfd486fbb1c4d
7inh <tranquoclinh27@gmail.com>
2022-07-16 21:50:33 +0700

    show notification

--- src/components/NotificationList.tsx
+++ src/components/NotificationList.tsx
@@ -1,9 +1,17 @@
 import { useCallback } from "react";
+import { useSelector } from "react-redux";
+import { selectLogin } from "../features/login/loginSlice";
 import useNotificationList from "../hooks/useNotificationList";
 
-
-export function NotificationItem({from, created_at}:any){
-    return(
+export function NotificationItem({
+  from_name,
+  from_avatar,
+  action,
+  job_title,
+  role,
+  created_at,
+}: any) {
+  return (
     <li>
       <span
         style={{
@@ -12,40 +20,45 @@ export function NotificationItem({from, created_at}:any){
           alignItems: "center",
         }}
       >
-        <img style={{ margin: 10, width: 40, height: 40 }} alt="img" />
-        <div
-          className="luca_profile_wrapper"
-          style={{ lineHeight: "25px", flexGrow: 1 }}
-        >
+        <img
+          style={{ margin: 10, width: 40, height: 40 }}
+          alt="img"
+          src={from_avatar}
+        />
+        <div style={{ lineHeight: "25px", flexGrow: 1, color: "black" }}>
           <div style={{ fontSize: 16 }}></div>
-          <div>{from}</div>
+          <p style={{ whiteSpace: "normal" }}>
+            <span style={{ color: "#ff3366"}}>{from_name}</span> <strong>{action}</strong> your{" "}
+            <strong>{job_title}</strong> {role==="candidate"?"application":"job"}
+          </p>
         </div>
       </span>
-    </li>);
+    </li>
+  );
 }
 export default function NotificationList() {
-    const notifications = useNotificationList();
+  const notifications = useNotificationList();
+  const { user } = useSelector(selectLogin);
 
-    const displayNotification = useCallback(() => {
-      return (
-        <ul className="list" style={{ width: 300 }}>
-          {notifications.map((notification: any, i: number) => {
-            return (
-              <NotificationItem
-                key={i}
-                from={notification.from}
-                created_at={notification.created_at}
-              />
-            );
-          })}
-          </ul>
-      );
-    }, [notifications]);
-
+  const displayNotification = useCallback(() => {
     return (
-        <>
-            {displayNotification()}
-        </>
-    )
-}
+      <ul className="list" style={{ width: 300 }}>
+        {notifications.map((notification: any, i: number) => {
+          return (
+            <NotificationItem
+              key={i}
+              from_name={notification.from_name}
+              from_avatar={notification.from_avatar}
+              action={notification.action}
+              job_title={notification.job_title}
+              created_at={notification.created_at}
+              role={user.role}
+            />
+          );
+        })}
+      </ul>
+    );
+  }, [notifications]);
 
+  return <>{displayNotification()}</>;
+}
--- src/hooks/useNotificationList.ts
+++ src/hooks/useNotificationList.ts
@@ -4,12 +4,13 @@ import { selectLogin } from "../features/login/loginSlice";
 
 const useNotificationList = () => {
   const { token } = useSelector(selectLogin);
-  const [notification, setNotifications] = useState([
-    {
-      from: "",
-      created_at: "",
-    },
-  ]);
+  const [notification, setNotifications] = useState<{
+    from_name: string;
+    from_avatar: string;
+    action: string;
+    job_title: string;
+    created_at: string;
+  }[]>([]);
 
   useEffect(() => {
     const getNotifications = async () => {
@@ -24,6 +25,8 @@ const useNotificationList = () => {
       };
 
       const response = await axios(config);
+
+      console.log("===========================", response.data);
 
       setNotifications(response.data);
     };
