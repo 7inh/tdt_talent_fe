57a0b1bf608e64148765fa068d53efd04e828344
Annn64778 <meobong1120@gmail.com>
2022-07-17 23:25:33 +0700

    lam edit footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,22 +12,22 @@ export default function Footer() {
                 <li>
                   <i className="flaticon-location-pointer" />
                   <p>
-                    123 City Avenue, Floor 10,
-                    <br /> malbourne, Australia.
+                   19 Nguyen Huu Tho
+                    <br /> Ho Chi Minh, Viet Nam
                   </p>
                 </li>
                 <li>
                   <i className="flaticon-telephone" />
                   <p>
-                    1 -234 -456 -7890
-                    <br /> 1 -234 -456 -7890
+                  (028) 37755046
+                    <br /> (028) 37755046
                   </p>
                 </li>
                 <li>
                   <i className="flaticon-envelope" />
-                  <a href="#0">info@Jbdesks.com </a>
+                  <a href="#0">Room C004 </a> 
                   <br />
-                  <a href="#0">support@Jbdesks.com</a>
+                  <a href="#0">Room C004</a>
                 </li>
               </ul>
               <ul className="icon_list_news jb_cover">
@@ -61,124 +61,26 @@ export default function Footer() {
                 <li>
                   <a href="#0">
                     <i className="fa fa-square" />
-                    Job Management &amp; Billing
+                    Job Management
                   </a>
                 </li>
                 <li>
                   <a href="#0">
                     <i className="fa fa-square" />
-                    Time &amp; Materials Tracking
+                    Company Management
                   </a>
                 </li>
                 <li>
                   <a href="#0">
                     <i className="fa fa-square" />
-                    Standards Compliance
+                    Account Management
                   </a>
                 </li>
-                <li>
-                  <a href="#0">
-                    <i className="fa fa-square" />
-                    Real Time GPS Tracking
-                  </a>
-                </li>
-                <li>
-                  <a href="#0">
-                    <i className="fa fa-square" />
-                    Client Portal
-                  </a>
-                </li>
-                <li>
-                  <a href="#0">
-                    <i className="fa fa-square" /> Powerful Workflow
-                  </a>
-                </li>
+          
               </ul>
             </div>
           </div>
-          <div className="col-lg-3 col-sm-6 col-12">
-            <div className="footerNav jb_cover footer_border_displ">
-              <h5>browse</h5>
-              <ul className="nav-widget">
-                <li>
-                  <a href="#0">
-                    <i className="fa fa-square" />
-                    Freelancers by Category
-                  </a>
-                </li>
-                <li>
-                  <a href="#0">
-                    <i className="fa fa-square" /> Freelancers in USA{" "}
-                  </a>
-                </li>
-                <li>
-                  <a href="#0">
-                    <i className="fa fa-square" /> Freelancers in UK
-                  </a>
-                </li>
-                <li>
-                  <a href="#0">
-                    <i className="fa fa-square" /> Freelancers in Canada
-                  </a>
-                </li>
-                <li>
-                  <a href="#0">
-                    <i className="fa fa-square" /> Freelancers in india
-                  </a>
-                </li>
-                <li>
-                  <a href="#0">
-                    <i className="fa fa-square" /> find jobs
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="col-lg-3 col-sm-6 col-12">
-            <div className="footerNav jb_cover footer_border_displ">
-              <h5>app &amp; integration</h5>
-              <ul className="nav-widget">
-                <li>
-                  <a href="#0">
-                    <img src="images/ft1.png" alt="img" />
-                    Xero
-                  </a>
-                </li>
-                <li>
-                  <a href="#0">
-                    <img src="images/ft2.png" alt="img" />
-                    Reckon
-                  </a>
-                </li>
-                <li>
-                  <a href="#0">
-                    <img src="images/ft3.png" alt="img" />
-                    Flexidocs
-                  </a>
-                </li>
-                <li>
-                  <a href="#0">
-                    <img src="images/ft4.png" alt="img" />
-                    Microsoft Exchange
-                  </a>
-                </li>
-                <li>
-                  <a href="#0">
-                    <img src="images/ft5.png" alt="img" /> Mailchimp
-                  </a>
-                </li>
-                <li>
-                  <a href="#0">
-                    <img src="images/ft6.png" alt="img" /> MYOB
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="copyright_left">
-            <i className="fa fa-copyright" /> 2019 <a href="#0"> JB desks.</a>{" "}
-            All Rights Reserved.
-          </div>
+
           <div className="clearfix" />
         </div>
         {/*/.row*/}
